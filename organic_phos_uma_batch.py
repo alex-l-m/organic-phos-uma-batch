e1022ffd1a29508cec3a9a92ce5be2331151fcc5
Alexander Davis <alexlovesmolecules@gmail.com>
2026-02-17 22:49:07 -0500

    Added geometry optimization with InferenceBatcher

--- organic_phos_uma_batch.py
+++ organic_phos_uma_batch.py
@@ -1,10 +1,12 @@
+from functools import partial
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import rdDistGeom
 from ase import Atoms
+from ase.optimize import BFGS
 from fairchem.core import pretrained_mlip
+from fairchem.core.calculate import FAIRChemCalculator, InferenceBatcher
 from fairchem.core.datasets.atomic_data import AtomicData, atomicdata_list_to_batch
-
 
 MODEL = 'uma-s-1p1'  # Models: uma-s-1p1, uma-m-1p1
 DEVICE = 'cpu'  # or 'cpu'
@@ -12,7 +14,16 @@ DEVICE = 'cpu'  # or 'cpu'
 # Load the pretrained model
 predictor = pretrained_mlip.get_predict_unit(MODEL, device=DEVICE) # Models: uma-s-1p1, uma-m-1p1
 
-def uma_batch_setup(multiplicity: int, predictor, atoms: Atoms):
+batcher = InferenceBatcher(predictor, concurrency_backend_options={'max_workers': 8})
+
+def uma_setup(multiplicity: int, predictor, atoms: Atoms) -> None:
+    '''Setup function for UMA for doing ASE calculations'''
+    atoms.info['charge'] = 0
+    atoms.info['spin'] = multiplicity
+    atoms.calc = FAIRChemCalculator(predictor, task_name='omol')
+
+def uma_atomicdata_setup(multiplicity: int, predictor, atoms: Atoms):
+    '''Setup function for UMA, the for using AtomicData for batching'''
     atoms.info['charge'] = 0
     atoms.info['spin'] = multiplicity
     return AtomicData.from_ase(atoms, task_name='omol', r_data_keys=['charge', 'spin'])
@@ -27,7 +38,14 @@ def initial_geometry(mol_id: str, smile: str) -> Atoms:
     atoms.info['name'] = mol_id
     return atoms
 
-# READ INPUT
+def triplet_geometry(predictor, atoms: Atoms) -> Atoms:
+    '''Optimize geometry using BFGS'''
+    uma_setup(multiplicity=3, predictor=predictor, atoms=atoms)
+    opt = BFGS(atoms)
+    opt.run(fmax=0.02)
+    return atoms
+
+# Read input
 intbl = pd.read_csv('smiles_energy.csv')
 mol_ids = intbl['mol_id'].tolist()
 smiles_strings = intbl['smiles'].tolist()
@@ -35,11 +53,23 @@ smiles_strings = intbl['smiles'].tolist()
 initial_geometries = [initial_geometry(mol_id, smile) \
         for mol_id, smile in zip(mol_ids, smiles_strings)]
 
-singlet_atomic_data = [uma_batch_setup(1, predictor, atoms) for atoms in initial_geometries]
+# Optimize geometries
+# Use InferenceBatcher
+# https://fair-chem.github.io/inference-batcher/
+# I think this is the way to do anything iterative in ASE like geometry
+# optimization or molecular dynamics
+optimized_geometries = list(batcher.executor.map(partial(triplet_geometry, batcher.batch_predict_unit), initial_geometries))
+
+# Calculate singlet-triplet gaps
+# Use atomic_data_list_to_batch
+# https://fair-chem.github.io/batch-inference/
+# Could use InferenceBatcher instead, but this works for energy or force
+# calculations
+singlet_atomic_data = [uma_atomicdata_setup(1, predictor, atoms) for atoms in optimized_geometries]
 singlet_batch = atomicdata_list_to_batch(singlet_atomic_data)
 singlet_energies = predictor.predict(singlet_batch)['energy'].tolist()
 
-triplet_atomic_data = [uma_batch_setup(3, predictor, atoms) for atoms in initial_geometries]
+triplet_atomic_data = [uma_atomicdata_setup(3, predictor, atoms) for atoms in optimized_geometries]
 triplet_batch = atomicdata_list_to_batch(triplet_atomic_data)
 triplet_energies = predictor.predict(triplet_batch)['energy'].tolist()
 
